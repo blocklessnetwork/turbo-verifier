9fba2923aca114ec33174ca1c7ee73f8f5acb205
Derek Anderson <dmikey@users.noreply.github.com>
2023-08-23 19:43:52 -0500

    simple tester

--- test.py
+++ test.py
@@ -3,5 +3,17 @@ import pywasm
 
 runtime = pywasm.load(
     './target/wasm32-unknown-unknown/release/tank_verifier.wasm')
-r = runtime.exec('verify_game_state', [10, 10, 10, 10, 10, 10, 10, 10, 10, 10])
-print(r)  # 55
+
+
+# tank1_x: i32, tank1_y: i32,
+# tank2_x: i32, tank2_y: i32,
+# shot1_x: i32, shot1_y: i32, shot1_status: i32,
+# shot2_x: i32, shot2_y: i32, shot2_status: i32
+
+r = runtime.exec('verify_game_state', [
+                 10, 10, 10, 10, 10, 10, 1, 10, 10, 1])
+print(r)  # 1, ok
+
+r = runtime.exec('verify_game_state', [
+                 10, 10, 10, 300, 200, 0, 10, 10, 10, 1])
+print(r)  # 0, fail
